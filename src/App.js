0416287ebce7058274a2f4ef4f2237e6702669e6
KhyeLerk <khyegit@gmail.com>
2022-12-02 14:55:48 +0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import nft_body from './nft-layers/nft_body.png';
+import nft_eyes_1 from './nft-layers/nft_eyes-1.png';
+import nft_eyes_2 from './nft-layers/nft_eyes-2.png';
+import nft_eyes_3 from './nft-layers/nft_eyes-3.png';
+import nft_hair_1 from './nft-layers/nft_hair-1.png';
+import nft_hair_2 from './nft-layers/nft_hair-2.png';
+import nft_hair_3 from './nft-layers/nft_hair-3.png';
+import nft_mouth_1 from './nft-layers/nft_mouth-1.png';
+import nft_mouth_2 from './nft-layers/nft_mouth-2.png';
+import nft_mouth_3 from './nft-layers/nft_mouth-3.png';
 
 function App() {
+  const NFTeyes = [nft_eyes_1, nft_eyes_2, nft_eyes_3]
+  const NFThair = [nft_hair_1, nft_hair_2, nft_hair_3]
+  const NFTmouth = [nft_mouth_1, nft_mouth_2, nft_mouth_3]
+  const [randomEyes, setRandomEyes] = useState(Math.floor(Math.random() * 3))
+  const [randomMouth, setRandomMouth] = useState(Math.floor(Math.random() * 3))
+  const [randomHair, setRandomHair] = useState(Math.floor(Math.random() * 3))
+
+  const random = (e) => {
+    e.preventDefault();
+    setRandomEyes(Math.floor(Math.random() * 3))
+    setRandomMouth(Math.floor(Math.random() * 3))
+    setRandomHair(Math.floor(Math.random() * 3))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="NFT">
+      <div className='nft-eyes'>
+        <img src={NFTeyes[randomEyes]} alt="eyes" />
+      </div>
+      <div className='nft-mouth'>
+        <img src={NFTmouth[randomMouth]} alt="mouth" />
+      </div>
+      <div className='nft-hair'>
+        <img src={NFThair[randomHair]} alt="hair" />
+      </div>
+      <div className='nft-body' >
+        <img src={nft_body} alt="body" />
+      </div>
+      <div className="Button">
+        <button className="Randomize" onClick={random}>Randomize</button>
+      </div>
     </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
